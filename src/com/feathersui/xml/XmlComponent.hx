42ddaa0f19d5724957148f3939ba0124dcd33c02
Josh Tynjala <joshtynjala@bowlerhat.dev>
2023-12-14 14:15:42 -0800

    XmlComponent: fix withMarkup code sample in docs

--- src/com/feathersui/xml/XmlComponent.hx
+++ src/com/feathersui/xml/XmlComponent.hx
@@ -40,11 +40,7 @@ import sys.io.File;
 		</f:LayoutGroup>'
 	);
 	container.addChild(instance);
-
-	var okButton = cast(instance.getChildAt(0));
-	//alternatively, use var okButton = XmlComponent.withMarkup('<f:Button xmlns:f="http://ns.feathersui.com/xml" id="okButton" text="OK"/>');
-	//then add it to instance
-	okButton.addEventListener(TriggerEvent.TRIGGER, (event) -> {
+	instance.okButton.addEventListener(TriggerEvent.TRIGGER, (event) -> {
 		trace("triggered the OK button");
 	});
 	```
